bb6a6ba83e4e366b12165f985ee7292c2a035b4a
avishka2k <avishka2k@gmail.com>
2023-08-04 23:01:22 +0530

    test user added

---
+++ lib/test_user_add.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AddUser extends StatelessWidget {
+  final String fullName;
+  final String company;
+  final int age;
+
+  AddUser(this.fullName, this.company, this.age);
+
+  @override
+  Widget build(BuildContext context) {
+    // Create a CollectionReference called users that references the firestore collection
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    Future<void> addUser() {
+      // Call the user's CollectionReference to add a new user
+      return users
+          .add({
+            'full_name': fullName, // John Doe
+            'company': company, // Stokes and Sons
+            'age': age // 42
+          })
+          .then((value) => print("User Added"))
+          .catchError((error) => print("Failed to add user: $error"));
+    }
+
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: addUser,
+          child: Text(
+            "Add User",
+          ),
+        ),
+      ),
+    );
+  }
+}
