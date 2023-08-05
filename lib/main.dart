ecd5e968d968392a8ed57b0fb9a379ea29774287
JanithaChamaka <JanithaChama2222@gmail.com>
2023-08-05 22:42:17 +0530

    update file and change tmp file location

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         primaryColor: const Color(0xFF18A689),
       ),
-      home: AddDataToFirebase(),
+      home: const AddDataToFirebase(),
     );
   }
 }
