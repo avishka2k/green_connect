b6ab24efc0d66a5179e1cb12f92e1c351cb2a706
TharushiVidanapathirana <malkividunima20050730@gmail.com>
2023-08-06 19:18:22 +0530

    change siede_menu path

--- lib/main_layer.dart
+++ lib/main_layer.dart
@@ -7,6 +7,7 @@ import 'package:green_connect/components/flutter_toast.dart';
 import 'package:green_connect/profile/profile_main.dart';
 import 'package:green_connect/side_menu/payments/payments_main.dart';
 import 'package:green_connect/side_menu/settings/settings_main.dart';
+import 'package:green_connect/side_menu/side_menu.dart';
 import 'app_color.dart';
 import 'calendar/calendar_main.dart';
 import 'package:flutter/material.dart';
@@ -35,155 +36,14 @@ class _MainLayerState extends State<MainLayer> {
     ProfileMain()
   ];
 
-  Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut();
-  }
-
-  Future<Map<String, dynamic>?> fetchUserData(User user) async {
-    try {
-      DocumentSnapshot<Map<String, dynamic>> userData = await FirebaseFirestore
-          .instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
-
-      if (userData.exists) {
-        return userData.data();
-      } else {
-        AppToastmsg.appToastMeassage('User data not found in Firestore');
-        return null;
-      }
-    } catch (e) {
-      AppToastmsg.appToastMeassage('Failed to fetch user data: $e');
-
-      return null;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    User? user = FirebaseAuth.instance.currentUser;
     return Scaffold(
       appBar: const AppBarWithMenu(
         title: "Add Remind",
         notifications: 4,
       ),
-      drawer: Drawer(
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            FutureBuilder<Map<String, dynamic>?>(
-                future: fetchUserData(user!),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else {
-                    final userData = snapshot.data;
-                    String username = userData!['username'] ?? 'No username';
-
-                    return UserAccountsDrawerHeader(
-                      accountName: Text(username),
-                      accountEmail: const Text("avishka@gmail.com"),
-                      currentAccountPicture: CircleAvatar(
-                        child: ClipOval(
-                          child: Image.network(
-                            'https://xsgames.co/randomusers/assets/avatars/male/77.jpg',
-                            // loadingBuilder: (context, child, loadingProgress) {
-                            //   if (loadingProgress == null) return child;
-                            //   return CircularProgressIndicator(
-                            //     value: loadingProgress.expectedTotalBytes !=
-                            //             null
-                            //         ? loadingProgress.cumulativeBytesLoaded /
-                            //             (loadingProgress.expectedTotalBytes ??
-                            //                 1)
-                            //         : null,
-                            //   );
-                            // },
-                            // errorBuilder: (context, error, stackTrace) {
-                            //   return const CircularProgressIndicator();
-                            // },
-                          ),
-                        ),
-                      ),
-                    );
-                  }
-                }),
-            AppMenuItem(
-              icon: "exam",
-              title: "Exam",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsMain(),
-                  ),
-                );
-              },
-            ),
-            AppMenuItem(
-              icon: "card",
-              title: "Payments",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const PaymentsMain(),
-                  ),
-                );
-              },
-            ),
-            AppMenuItem(
-              icon: "settings",
-              title: "Settings",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsMain(),
-                  ),
-                );
-              },
-            ),
-            AppMenuItem(
-              icon: "enq",
-              title: "Enquiry Center",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsMain(),
-                  ),
-                );
-              },
-            ),
-            AppMenuItem(
-              icon: "help",
-              title: "Help",
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsMain(),
-                  ),
-                );
-              },
-            ),
-            AppMenuItem(
-              icon: "out",
-              title: "Log Out",
-              onTap: signOut,
-            )
-          ],
-        ),
-      ),
+      drawer: const SideMenu(),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
---
+++ lib/side_menu/side_menu.dart
@@ -0,0 +1,164 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:green_connect/components/app_menu_item.dart';
+import 'package:green_connect/side_menu/payments/payments_main.dart';
+import 'package:green_connect/side_menu/settings/settings_main.dart';
+
+import '../components/flutter_toast.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Future<Map<String, dynamic>?> fetchUserData(User user) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> userData = await FirebaseFirestore
+          .instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      if (userData.exists) {
+        return userData.data();
+      } else {
+        AppToastmsg.appToastMeassage('User data not found in Firestore');
+        return null;
+      }
+    } catch (e) {
+      AppToastmsg.appToastMeassage('Failed to fetch user data: $e');
+
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
+    return Drawer(
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          FutureBuilder<Map<String, dynamic>?>(
+              future: fetchUserData(user!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else {
+                  final userData = snapshot.data;
+                  String username = userData!['username'] ?? 'No username';
+
+                  return UserAccountsDrawerHeader(
+                    accountName: Text(username),
+                    accountEmail: const Text("avishka@gmail.com"),
+                    currentAccountPicture: CircleAvatar(
+                      child: ClipOval(
+                        child: Image.network(
+                          'https://xsgames.co/randomusers/assets/avatars/male/77.jpg',
+                          // loadingBuilder: (context, child, loadingProgress) {
+                          //   if (loadingProgress == null) return child;
+                          //   return CircularProgressIndicator(
+                          //     value: loadingProgress.expectedTotalBytes !=
+                          //             null
+                          //         ? loadingProgress.cumulativeBytesLoaded /
+                          //             (loadingProgress.expectedTotalBytes ??
+                          //                 1)
+                          //         : null,
+                          //   );
+                          // },
+                          // errorBuilder: (context, error, stackTrace) {
+                          //   return const CircularProgressIndicator();
+                          // },
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              }),
+          AppMenuItem(
+            icon: "exam",
+            title: "Exam",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsMain(),
+                ),
+              );
+            },
+          ),
+          AppMenuItem(
+            icon: "card",
+            title: "Payments",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const PaymentsMain(),
+                ),
+              );
+            },
+          ),
+          AppMenuItem(
+            icon: "settings",
+            title: "Settings",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsMain(),
+                ),
+              );
+            },
+          ),
+          AppMenuItem(
+            icon: "enq",
+            title: "Enquiry Center",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsMain(),
+                ),
+              );
+            },
+          ),
+          AppMenuItem(
+            icon: "help",
+            title: "Help",
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsMain(),
+                ),
+              );
+            },
+          ),
+          AppMenuItem(
+            icon: "out",
+            title: "Log Out",
+            onTap: signOut,
+          )
+        ],
+      ),
+    );
+  }
+}
