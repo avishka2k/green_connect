848e141faeead2794ee1bc58945ef436b32e42b2
Hiruni2000 <Shhiruninawoda@gmail.com>
2023-08-19 21:16:04 +0530

    community_add update

---
+++ lib/community_pages/community_add.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommunityAdd extends StatelessWidget {
+  const CommunityAdd ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
