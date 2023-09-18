1d9b9205b652c3ac7c4c9eae4968683032d1b009
Janidu Wathsara <wathsarajanidu@gmail.com>
2023-09-18 21:59:05 +0530

    Modified Enter ID page

--- lib/payment/Enter_Id.dart
+++ lib/payment/Enter_Id.dart
@@ -1,60 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+  const MyHomePage({super.key});
 
   get _textEditingController => null;
 
-  List<String> items = [
-    '',
-    'Degree fee',
-    'Bridging Programme fee',
-    'Hostel fee'
-  ];
-  String? selectedItem = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(title: Text("Payments",
         style: TextStyle(
           color: Colors.green[800],
           fontWeight: FontWeight.bold,
+
         ) ,
       ),
 
-      appBar: AppBar(
-        title: Text(
-          "Payments",
-          style: TextStyle(
-            color: Colors.green[800],
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        // flexibleSpace:Image.asset("assests/images(2).jpeg",
-        //   fit: BoxFit.cover,
-        // ),
-
-
         elevation: 1.0,
         backgroundColor: Colors.white,
+
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
 
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 15),
+            const SizedBox(height: 30),
             const Align(
-              alignment: FractionalOffset(0, 0.2),
+              alignment: FractionalOffset(0, 1),
               child: Text(
-                'Details                             ',
+                'Pay online',
                 textAlign: TextAlign.start,
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 25,
                   fontWeight: FontWeight.bold,
                   color: Colors.green,
                 ),
@@ -62,61 +60,48 @@ class MyHomePage extends StatelessWidget {
             ),
 
 
-            // Enter name ----------------------------------------------------
+            // Text ...............................
 
-            const SizedBox(height: 15),
+            const SizedBox(height: 20),
             const Align(
-              alignment: FractionalOffset(0, 0.2),
+              alignment: FractionalOffset(0, 1),
               child: Text(
-                "Student Name : ",
-                textAlign: TextAlign.start,
+                'Online payments',
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize:18,
                   fontWeight: FontWeight.bold,
                   color: Colors.green,
                 ),
               ),
             ),
 
-            const SizedBox(height: 0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  width: 450,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: TextField(
-                      decoration: const InputDecoration(hintText: 'your name'),
-                      controller: _textEditingController,
-                      textAlign: TextAlign.start,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
-
-                      ),
-                      keyboardType: TextInputType.text,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.singleLineFormatter,
-                      ],
-                      onChanged: (value) {},
-                    ),
-                  ),
-                ),
-              ],
+            const Divider(
+              color: Colors.black45,
+              thickness: 1,
+              indent: 2,
+              endIndent: 2,
             ),
-
-            // Enter intake --------------------------------------------------
 
             const SizedBox(height: 10),
             const Align(
               alignment: FractionalOffset(0, 0.2),
               child: Text(
-                'Intake :',
+                ' Enter your student ID and verify details.',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black54
+                ),
+              ),
+            ),
+
+
+            // Enter ID ...............................
+
+            const SizedBox(height: 45),
+            const Align(
+              alignment: FractionalOffset(0, 0.2),
+              child: Text(
+                "Student's NSBM ID no/Reference no: ",
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
@@ -125,12 +110,12 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
 
-            const SizedBox(height: 0),
+            const SizedBox(height: 5),
             Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Container(
-                  width: 450,
+                  width: 370,
                   decoration: BoxDecoration(
                     border: Border.all(color: Colors.green),
                     borderRadius: BorderRadius.circular(4),
@@ -138,17 +123,14 @@ class MyHomePage extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8),
                     child: TextField(
-                      decoration: const InputDecoration(hintText: 'batch'),
+                      decoration: const InputDecoration(hintText: 'Enter Student ID'),
                       controller: _textEditingController,
                       textAlign: TextAlign.start,
                       style: const TextStyle(
-                        fontSize: 18,
+                        fontSize: 17,
                         fontWeight: FontWeight.normal,
                       ),
                       keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.singleLineFormatter,
-                      ],
                       onChanged: (value) {},
                     ),
                   ),
@@ -157,216 +139,31 @@ class MyHomePage extends StatelessWidget {
             ),
 
 
-            // Enter degree --------------------------------------------------
+            // Search button .............................
 
-            const SizedBox(height: 10),
-            const Align(
-              alignment: FractionalOffset(0, 0.2),
-              child: Text(
-                'Degree :',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-                ),
-              ),
-            ),
-
-
-            const SizedBox(height: 0),
+            const SizedBox(height: 5),
             Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Container(
-                  width: 450,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: TextField(
-
-                      decoration: const InputDecoration(hintText: 'degree name'),
-
-                      controller: _textEditingController,
-                      textAlign: TextAlign.start,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
-                      ),
-                      keyboardType: TextInputType.text,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.singleLineFormatter,
-                      ],
-                      onChanged: (value) {},
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            // Enter fee type ------------------------------------------------
-
-            const SizedBox(height: 10),
-            const Align(
-              alignment: FractionalOffset(0, 0.2),
-              child: Text(
-                'Fee Type :',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-                ),
-              ),
-            ),
-
-
-            const SizedBox(height: 0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                SizedBox(
-                  width: 400,
-                  height: 58,
-                  child: DropdownButtonFormField<String>(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-
-                        borderSide: const BorderSide(width: 1.5, color: Colors.green),
-                      ),
-                    ),
-                    value: selectedItem,
-                    items: items.
-                    map((item) => DropdownMenuItem<String>(
-                      value: item,
-                      child: Text(item, style: const TextStyle(fontSize: 16),),
-                    ))
-                        .toList(),
-                    onChanged: (item) => setState(() => selectedItem = item),
-                  ),
-                ),
-              ],
-            ),
-
-
-            // Enter amount -------------------------------------------------
-
-            const SizedBox(height: 10),
-            const Align(
-              alignment: FractionalOffset(0, 0.2),
-              child: Text(
-                'Amount :',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-
-                        borderSide:
-                            const BorderSide(width: 1.5, color: Colors.green),
-                      ),
-                    ),
-                    value: selectedItem,
-                    items: items
-                        .map((item) => DropdownMenuItem<String>(
-                              value: item,
-                              child: Text(
-                                item,
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                            ))
-                        .toList(),
-                    onChanged: (item) => setState(() => selectedItem = item),
-                  ),
-
-                ),
-              ],
-            ),
-
-            // Enter amount -------------------------------------------------
-
-            const SizedBox(height: 10),
-            const Align(
-              alignment: FractionalOffset(0, 0.2),
-              child: Text(
-                'Amount :',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  width: 450,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: TextField(
-                      decoration: const InputDecoration(hintText: 'LKR'),
-                      controller: _textEditingController,
-                      textAlign: TextAlign.start,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
-                      ),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.singleLineFormatter,
-                      ],
-                      onChanged: (value) {},
-                    ),
-                  ),
-                ),
-
-
-                // Pay now button
-
-
-                const SizedBox(height: 35),
+                const SizedBox(height: 25),
                 ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(150, 50),
+                    minimumSize: const Size(450, 50),
                   ),
                   child: const Text(
-                    'Pay Now',
+                    'SEARCH',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-
               ],
             ),
-
-
           ],
         ),
-
       ),
     );
-
-
-    // TabBarView(
-    //   children: [
-    //     tab1(),
-    //     Icon(Icons.directions_transit),
-    //     Icon(Icons.directions_bike),
-    //   ],
-    // ),
-
   }
 }
-
-setState(String? Function() param0) {}
